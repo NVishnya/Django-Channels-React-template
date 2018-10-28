a1395052cb31535a919070e9e91540ca772e98e1
Nastya Vishnya <stacyvishnya@gmail.com>
2018-10-28 17:24:05 +0200

    fix http://127.0.0.1:8000/ Page not found

--- chat/routing.py
+++ chat/routing.py
@@ -2,5 +2,5 @@ from django.urls.conf import path
 from chat import consumers
 
 websocket_urlpatterns = [
-    path('ws/chat/<room_name>/', consumers.ChatConsumer),
+    path('ws/<room_name>/', consumers.ChatConsumer),
 ]
--- chat/urls.py
+++ chat/urls.py
@@ -2,7 +2,7 @@ from chat.views import IndexView, RoomView, MessagesListView
 from django.urls.conf import path
 
 urlpatterns = [
-    path('chat/', IndexView.as_view(), name='index'),
-    path('chat/<room_name>/', RoomView.as_view(), name='room'),
+    path('', IndexView.as_view(), name='index'),
+    path('<room_name>/', RoomView.as_view(), name='room'),
     path('api/messages/<room_name>/', MessagesListView.as_view()),
 ]
