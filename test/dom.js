9f85a55cb7749e2de6228eca1e7cada39a5a449a
Nastya Liashchevska <nnvishnya@gmail.com>
2019-07-18 15:47:47 +0300

    Remove frontend part

--- test/dom.js
+++
@@ -1,6 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
-
-global.window = dom.window;
-global.document = dom.window.document;
