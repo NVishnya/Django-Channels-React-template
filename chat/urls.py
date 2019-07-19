f1c7e40d5b0e8ed43c485b3ea94c3f067f2807c9
Nastya Liashchevska <nnvishnya@gmail.com>
2019-07-19 13:24:40 +0300

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -3,6 +3,6 @@ from django.urls.conf import path
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('<room_name>/', RoomView.as_view(), name='room'),
+    path('<room_name>/', IndexView.as_view(), name='room'),
     path('api/messages/<room_name>/', MessagesListView.as_view()),
 ]
